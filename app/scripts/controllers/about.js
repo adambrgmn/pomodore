0e4750466a2ef98898dc044ed5e311d85d11cbf4
Adam Bergman <brgmn.adam@gmail.com>
2015-12-07 10:42:09 +0100

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pomodoreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pomodoreApp
+ */
+angular.module('pomodoreApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pomodoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pomodoreApp
+ */
+angular.module('pomodoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
