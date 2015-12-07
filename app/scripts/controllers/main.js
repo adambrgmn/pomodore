35b3a9fe787396bd6f9c65fa1be3ede86f28fc69
Adam Bergman <brgmn.adam@gmail.com>
2015-12-07 18:02:19 +0100

    Getting on show

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,69 @@
  * Controller of the pomodoreApp
  */
 angular.module('pomodoreApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $interval) {
+    var vm = this;
+    var stop;
+    var soundLoop = 2;
+    var alarm = new Howl({
+      urls: ['audio/alarm.mp3'],
+      autoplay: false,
+      loop: false,
+      onend: function () {
+        if(soundLoop > 0) {
+          soundLoop -= 1;
+          alarm.play();
+        } else {
+          alarm.stop();
+        }
+      }
+    });
+
+    vm.timer = 0;
+    vm.playing = false;
+    vm.paused = false;
+    vm.progress = 100;
+
+    vm.pomodore = function (value, newTimer) {
+      vm.playing = true;
+      vm.paused = 'Pause';
+
+      if (angular.isDefined(stop)) { return; }
+
+      if (newTimer || vm.timer === 0) {
+        vm.timer = value;
+        vm.part = 100/(value/1000);
+      }
+
+      stop = $interval(function () {
+        if (vm.timer > 0) {
+          vm.progress -= vm.part;
+          vm.timer -= 1000;
+        } else {
+          alarm.play();
+          vm.stopPomodore();
+          vm.resetPomodore();
+        }
+      }, 1000);
+    };
+
+    vm.stopPomodore = function () {
+      if (angular.isDefined(stop)) {
+        $interval.cancel(stop);
+        vm.paused = 'Resume';
+        stop = undefined;
+      } else {
+        vm.pomodore(0, false);
+      }
+    };
+
+    vm.resetPomodore = function () {
+      vm.timer = 0;
+      vm.progress = 100;
+      vm.playing = false;
+    };
+
+    $scope.$on('$destroy', function () {
+      vm.stopTimer();
+    });
   });
