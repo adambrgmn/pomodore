dfc667ef00ecbbe3e307c96f061c5e44d926b0ff
Adam Bergman <brgmn.adam@gmail.com>
2016-02-08 12:47:44 +0100

    New structure

---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('pomodoreApp', [
+  'pomodoreApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
