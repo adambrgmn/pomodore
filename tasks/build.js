69d882c4515358408ad1c735d9ca3175e1c0fe0a
Adam Bergman <brgmn.adam@gmail.com>
2016-02-12 16:27:52 +0100

    Updated build method

--- tasks/build.js
+++ tasks/build.js
@@ -47,7 +47,7 @@ gulp.task('clean:finish', function (done) {
 });
 
 gulp.task('copy:dist', function () {
-  var main = gulp.src(['server/**/*', 'package.json'], { base: './' });
+  var main = gulp.src(['server/**/*', 'package.json', 'ecosystem.json'], { base: './' });
   var assets = gulp.src('client/assets/**/*', { base: './' });
 
   return sq({ objectMode: true }, main, assets)
